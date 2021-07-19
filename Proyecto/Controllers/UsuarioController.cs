eba911ca2d75eca699a411f113a6f54284784b64
lanbCR <lanb199524@gmail.com>
2021-07-19 09:50:31 -0600

    template

---
+++ Proyecto/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Entidades;
+
+namespace Proyecto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet] // Nos sirve para entrar a las vistas
+        public ActionResult ConsultaUsuarios()
+        {
+
+            return View();
+        }
+
+        public ActionResult IngresoUsuarios()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegistrarUsuario(UsuarioObj obj)
+        {
+
+            return RedirectToAction("IngresoUsuarios", "Usuario");
+        }
+
+        public ActionResult RegistrarUsuarioJS()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Proyecto/FuncionesJS/UsuarioJS.js
@@ -0,0 +1,16 @@
+﻿window.onload = Inicio();
+
+function Inicio() {
+    document.getElementById("userID").placeholder = "User ID";
+    document.getElementById("Correo").placeholder = "Correo";
+    document.getElementById("Contrasena").placeholder = "Contraseña";
+}
+
+function RegistrarUsuario() {
+
+    var id = document.getElementbyId("userID").value;
+    var email = document.getElementbyId("Correo").value;
+    var paasword = $("Contrasena").val();
+
+}
+
